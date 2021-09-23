53c7dba95a768858b5ed0e87e08ce1999aff2ba8
manavk09 <mk1745@scarletmail.rutgers.edu>
2021-09-23 15:41:45 -0400

    Song Object Created

---
+++ SongLib/src/SongLib/App/Song.java
@@ -0,0 +1,44 @@
+package SongLib.App;
+
+public class Song {
+	private String songName;
+	private String artistName;
+	private int year;
+	private String album;
+	
+	public Song(String songName, String artistName, int year, String album) {
+		this.songName = songName;
+		this.artistName = artistName;
+		this.year = year;
+		this.album = album;
+	}
+	
+	public void setName(String name) {
+		this.songName = name;
+	}
+	public String getSongName() {
+		return songName;
+	}
+	public void setArtistName(String name) {
+		this.artistName = artistName;
+	}
+	public String getArtistName() {
+		return artistName;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	public String getAlbum() {
+		return album;
+	}
+	public String toString() {
+		return songName + " " + artistName;
+	}
+	
+}
