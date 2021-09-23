b85d79c80a5af47dbd117d4029ebf9556d23dc6f
manavk09 <mk1745@scarletmail.rutgers.edu>
2021-09-23 18:23:54 -0400

    File Done

--- SongLib/src/SongLib/App/SongLibApp.java
+++ SongLib/src/SongLib/App/SongLibApp.java
@@ -2,11 +2,14 @@ package SongLib.App;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 import SongLib.View.SongLibController;
 import javafx.application.Application;
@@ -21,11 +24,7 @@ public class SongLibApp extends Application{
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		readFromfile("/SongLib/App/songList");
-		//Load the songList
-		//songList.add(new Song("songname", "artistname", 2069, "albumyee"));
-		//songList.add(new Song("songname2", "artistname2", 2222, "alsadfsd"));
-		
+		readFromfile();
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass().getResource("/SongLib/View/SongLibUI.fxml"));
 		System.out.println(loader.getLocation());
@@ -39,18 +38,26 @@ public class SongLibApp extends Application{
 		primaryStage.show();
 		
 	}
-	public void readFromfile(String filePath) throws IOException {
-		File file = new File(filePath);
-		  
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		  
-		String st;
-		while ((st = br.readLine()) != null) {
-			List<String> temp = Arrays.asList(st.split("\t"));
-			songList.add(new Song(temp.get(0),temp.get(1),Integer.parseInt(temp.get(2)),temp.get(3)));		  
+	public void readFromfile(){
+		String file = "src/songList.txt";
+	   
+		Scanner sc;
+		try {
+			sc = new Scanner(new File(file));
+			sc.useDelimiter("\n");
+		    
+		    while (sc.hasNextLine()) {
+		    	String t = sc.nextLine();
+		    	List<String> temp = Arrays.asList(t.split("\t"));
+				songList.add(new Song(temp.get(0),temp.get(1),Integer.parseInt(temp.get(2)),temp.get(3)));		
+		    }
+		    sc.close();
+		} 
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		br.close();
+	    
 
 	}
 	
