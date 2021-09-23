488118e8bcde791bf433dcaa1c72bdd7ba64023e
manavk09 <mk1745@scarletmail.rutgers.edu>
2021-09-23 12:20:28 -0400

    Commit

--- SongLib/src/SongLib/View/SongLibController.java
+++ SongLib/src/SongLib/View/SongLibController.java
@@ -1,5 +1,33 @@
 package SongLib.View;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class SongLibController {
+	
+	@FXML Button add;
+	@FXML Button delete;
+	@FXML Button edit;
+	@FXML Button save;
+	@FXML Button cancel;
+	
+	public void addSong(ActionEvent e) {
+		
+	}
+	public void deleteSong(ActionEvent e) {
+		
+	}
+	public void editSongInfo(ActionEvent e) {
+		
+	}
+	public void saveEdit(ActionEvent e) {
+		
+	}
+	public void cancelEdit(ActionEvent e) {
+		
+	}
+	
 
 }
