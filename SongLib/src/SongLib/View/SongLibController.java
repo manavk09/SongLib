3d921d15cdc6e90d623495018c904080278dd2b4
kritikp <ksp127@scarletmail.rutgers.edu>
2021-09-23 18:24:48 -0400

    Base Edit Working

--- SongLib/src/SongLib/View/SongLibController.java
+++ SongLib/src/SongLib/View/SongLibController.java
@@ -6,9 +6,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 import javafx.event.*;
 
@@ -28,8 +30,10 @@ public class SongLibController {
 	@FXML ListView<Song> songList;
 	
 	public ObservableList<Song> obsSongList;
+	public Stage mainStage;
 	
 	public void start(Stage primaryStage) {
+		mainStage = primaryStage;
 		obsSongList = FXCollections.observableArrayList(SongLibApp.songList);
 		songList.setItems(obsSongList);
 		
@@ -52,9 +56,14 @@ public class SongLibController {
 		artistField.setText(artist);
 		albumField.setText(album);
 		yearField.setText("" + year);
+		
+		//Enable and disable fields and buttons appropriately
 		setFieldsWritable(false);
+		cancelButton.setDisable(true);
+		saveButton.setDisable(true);
 	}
 
+	//Set if fields can be edited
 	public void setFieldsWritable(boolean isWritable) {
 		nameField.setEditable(isWritable);
 		artistField.setEditable(isWritable);
@@ -65,15 +74,46 @@ public class SongLibController {
 	public void addSong(ActionEvent e) {
 		
 	}
+	
 	public void deleteSong(ActionEvent e) {
 		
 	}
+	
 	public void editSongInfo(ActionEvent e) {
+		//Enable and disable fields and buttons appropriately
 		setFieldsWritable(true);
-	}
-	public void saveEdit(ActionEvent e) {
+		editButton.setDisable(true);
+		addButton.setDisable(true);
+		deleteButton.setDisable(true);
+		cancelButton.setDisable(false);
+		saveButton.setDisable(false);
 		
 	}
+	
+	public void saveEdit(ActionEvent e) {
+		songList.getSelectionModel().getSelectedItem().setName(nameField.getText());
+		songList.getSelectionModel().getSelectedItem().setAlbumName(albumField.getText());
+		songList.getSelectionModel().getSelectedItem().setArtistName(artistField.getText());
+		try {
+			int year = Integer.parseInt(yearField.getText());
+			songList.getSelectionModel().getSelectedItem().setYear(year);
+		}
+		catch(Exception ex) {
+			yearField.setText("" + songList.getSelectionModel().getSelectedItem().getYear());
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.initOwner(mainStage);
+			alert.setTitle("Error");
+			alert.setHeaderText("Invalid input! Make sure you don't have the '|' character anywhere and only numbers for the year.");
+			alert.showAndWait();
+		}
+		setFieldsWritable(false);
+		editButton.setDisable(false);
+		addButton.setDisable(false);
+		deleteButton.setDisable(false);
+		cancelButton.setDisable(true);
+		saveButton.setDisable(true);
+	}
+	
 	public void cancelEdit(ActionEvent e) {
 		
 	}
