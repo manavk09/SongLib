6df9b48b9754aa1488c33abff2e50f4161d89955
manavk09 <mk1745@scarletmail.rutgers.edu>
2021-09-23 17:16:03 -0400

    FileReaderAdd

--- SongLib/src/SongLib/App/SongLibApp.java
+++ SongLib/src/SongLib/App/SongLibApp.java
@@ -1,7 +1,14 @@
-package SongLib.App;""
+package SongLib.App;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import SongLib.View.SongLibController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,22 +17,39 @@ import javafx.stage.Stage;
 
 public class SongLibApp extends Application{
 
-	public static ArrayList<Song> songList;
-	
+	public static ArrayList<Song> songList = new ArrayList<Song>();
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
 		//Load the songList
 		songList.add(new Song("songname", "artistname", 2069, "albumyee"));
+		songList.add(new Song("songname2", "artistname2", 2222, "alsadfsd"));
 		
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass().getResource("/SongLib/View/SongLibUI.fxml"));
 		System.out.println(loader.getLocation());
 		VBox root = (VBox) loader.load();
+		
+		SongLibController songLibController = loader.getController();
+		songLibController.start(primaryStage);
+		
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+	}
+	public void readFromfile(String filePath) throws IOException {
+		File file = new File(filePath);
+		  
+		  BufferedReader br = new BufferedReader(new FileReader(file));
+		  
+		  String st;
+		  while ((st = br.readLine()) != null) {
+			  List<String> temp = Arrays.asList(st.split("\t"));
+			  songList.add(new Song(temp.get(0),temp.get(1),Integer.parseInt(temp.get(2)),temp.get(3)));		  
+		  }
+		
 		
 	}
 	
--- SongLib/src/SongLib/View/SongLibController.java
+++ SongLib/src/SongLib/View/SongLibController.java
@@ -1,9 +1,15 @@
 package SongLib.View;
 
+import SongLib.App.SongLibApp;
+import SongLib.App.Song;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class SongLibController {
 	
@@ -13,6 +19,38 @@ public class SongLibController {
 	@FXML Button save;
 	@FXML Button cancel;
 	
+	@FXML TextField nameField;
+	@FXML TextField artistField;
+	@FXML TextField albumField;
+	@FXML TextField yearField;
+	@FXML ListView<Song> songList;
+	
+	public ObservableList<Song> obsSongList;
+	
+	public void start(Stage primaryStage) {
+		obsSongList = FXCollections.observableArrayList(SongLibApp.songList);
+		songList.setItems(obsSongList);
+		
+		songList.getSelectionModel().select(0);
+		showItem(primaryStage);
+		
+		songList.getSelectionModel().selectedIndexProperty()
+		.addListener((obs, oldval, newval) -> showItem(primaryStage));
+	}
+	
+	private void showItem(Stage primaryStage) {
+		String name, artist, album;
+		int year;
+		name = songList.getSelectionModel().getSelectedItem().getSongName();
+		artist = songList.getSelectionModel().getSelectedItem().getArtistName();
+		album = songList.getSelectionModel().getSelectedItem().getAlbumName();
+		year = songList.getSelectionModel().getSelectedItem().getYear();
+		nameField.setText(name);
+		artistField.setText(artist);
+		albumField.setText(album);
+		yearField.setText("" + year);
+	}
+
 	public void addSong(ActionEvent e) {
 		
 	}
